5b50383e01b0593a90b711cc669a3dc1833981cd
jameshills6502 <94471227+jameshills6502@users.noreply.github.com>
2024-06-30 21:48:46 +0100

    Add files via upload

--- file-hashing.py
+++ file-hashing.py
@@ -1,28 +1,57 @@
 import sys
 import hashlib
-import pandas
-import sqlite3
 import mysql.connector
+from mysql.connector import errorcode
 import datetime 
-import time 
+import ttkbootstrap as ttk 
+
 
 buffer_size = 65536 #runs better with a buffer
 
 md5 = hashlib.md5()
 sha1 = hashlib.sha1()
 
+def initial_setup():
+    global mycursor
+    global mydb
+    # not defining these across the program
+    try:
+        mydb = mysql.connector.connect(host ="localhost", db ="hashtable", user="app", password="ForensicApp@018")
+        ## HOW CAN I MAKE THIS MORE SECURE?
+        mycursor = mydb.cursor()
+        window_create()
+    except mysql.connector.Error:
+        print("Error! Database not found! \nExiting program")
+        quit()
+    
+#### CREATE STARTING WINDOW ####
+def window_create():
+    app = ttk.Window(themename="solar")
+    app.geometry("800x300")
+    label = ttk.Label(app, text="FILE INTEGRITY CHECKER")
+    entry1label = ttk.Label(app, text="Enter your username:")
+    entry2label = ttk.Label(app, text="Enter your password:")
+    entry1label.grid(row=1, column=0, pady=2)
+    entry2label.grid(row=2, column=0, pady=2)
+    username_entry = ttk.Entry(app)
+    password_entry = ttk.Entry(app, show="*")
+    login_error = ttk.Label(app, text="Error!\nPassword or username is incorrect")
+    submit_button = ttk.Button(app, text="Submit", command= lambda: login(username_entry, password_entry, login_error))
+    account_button = ttk.Button(app, text="Create account", command= create_account_page)
+    quit_button = ttk.Button(app, text="Quit", command= lambda: destroy(app))
+    label.grid(row=0, column = 1, pady = 1)
+    username_entry.grid(row=1, column=1,pady=1)
+    password_entry.grid(row=2, column=1,pady=1)
+    quit_button.grid(row=3, column = 2, pady =2)
+    submit_button.grid(row=3, column =1, pady= 1)
+    account_button.grid(row=3, column =0, pady=1)
+    app.mainloop()
 
+def destroy(app):
+    app.destroy()
 
-mydb = mysql.connector.connect(host ="localhost", db ="hashtable", user="app", password="ForensicApp@018")
-mycursor = mydb.cursor()
-
-
-
-def file_name():
-    user_input = input("Please input filename:")
-    logic(user_input)
+#### FILE HANDLING AND HASHING ####
 def logic(filename):
-    print(filename)
     try:
         f = open(filename, "rb")
         while True:
@@ -34,14 +63,85 @@ def logic(filename):
             searching(filename, md5.hexdigest(), sha1.hexdigest())
     except FileNotFoundError:
         print("File not found!")
-    
-def storing(filename, md5hash, sha1hash):
-    sql = "INSERT INTO forensic_app(`date_of_scan`, `file_name`, `md5_sum`, `sha1_sum`) VALUES(%s, %s, %s, %s);"
+
+#### TTK CODE FOR CREATE ACCOUNT PAGE ####
+def create_account_page():
+    top = ttk.window.Toplevel()
+    top.title('Create Account')
+    top.grab_set()
+    l1 = ttk.Label(top, text="Enter Username:")
+    l4 = ttk.Label(top, text="Enter Password:")
+    l5 = ttk.Label(top, text="Confirm Password:")
+    username = ttk.Entry(top, width=50)
+    password = ttk.Entry(top, width=50, show="*")
+    confirm_password = ttk.Entry(top, width=50, show="*")
+    error = ttk.Label(top, text="Passwords don't match!")
+    success = ttk.Label(top, text="Account Created!")
+    submit = ttk.Button(top, text="Create Account", command= lambda: store_account(username, password, confirm_password, error, success))
+    l1.grid(row=0,column=0, pady=2)
+    l4.grid(row=1, column=0, pady=2)
+    l5.grid(row=2, column=0, pady=2)
+    username.grid(row=0, column=1, pady=2)
+    password.grid(row=1, column=1, pady=2)
+    confirm_password.grid(row=2, column=1, pady=2)
+    submit.grid(row=3, column=1, pady=1)
+
+
+#### SEARCHING FOR USERNAME PRESENCE BEFORE ADDING TO DATABASE, STORING USER_ID FOR LATER USE ####
+def search_acc(username):
+    sql = "SELECT user_id FROM user_accounts WHERE username = %s"
+    user = (username, )
+    mycursor.execute(sql, user)
+    data = mycursor.fetchall()
+    if len(data) == 0:
+        return None
+    else:
+        return data
+        ## USE THIS FOR LATER!
+
+#### LOGIN FUNCTION ####
+def login(username_entry, password_entry, error):
+    username = username_entry.get()
+    password = password_entry.get()
+    sql = "SELECT * FROM user_accounts WHERE username = %s AND password = %s;"
+    val = (username, password)
+    mycursor.execute(sql, val)
+    data = mycursor.fetchall()
+    if len(data) == 0:
+        error.grid(row=4, column=1, pady=1)
+    else:
+        print("File exists in database!")
+        print(data)
+
+#### STORE USER ACCOUNT FUNCTION ####
+def store_account(username_entry, password_entry, confirm_password_entry, error, success):
+    username = username_entry.get()
+    password = password_entry.get()
+    confirm_password = confirm_password_entry.get()
+    if password == confirm_password:
+        does_account_exist = search_acc(username)
+        if does_account_exist == None:
+            sql = "INSERT INTO user_accounts(`username`, `password`) VALUES(%s, %s);"
+            val = (username, password)
+            mycursor.execute(sql, val)
+            mydb.commit()
+            success.grid(row=4,column=1, pady=1)
+        else:
+            print(does_account_exist)
+            for i in does_account_exist:
+                print(i)
+    else:
+        error.grid(row=4, column=1, pady=1)
+
+#### STORE FILE HASHES FUNCTION ####
+def store_file_data(filename, md5hash, sha1hash, user_id):
+    sql = "INSERT INTO forensic_app(`date_of_scan`, `file_name`, `md5_sum`, `sha1_sum`, `user_id`) VALUES(%s, %s, %s, %s, %s);"
     date_of_scan = datetime.datetime.now()
-    val = (date_of_scan, filename, md5hash, sha1hash)
+    val = (date_of_scan, filename, md5hash, sha1hash, user_id)
     mycursor.execute(sql, val)
     mydb.commit()
 
+#### SEARCHING ####
 def searching(filename, md5_hash, sha1_hash):
     print("Checking database...")
     hashes = str(md5_hash)
@@ -49,16 +149,12 @@ def searching(filename, md5_hash, sha1_hash):
     val = (hashes, )
     mycursor.execute(sql, val)
     data = mycursor.fetchall()
-    
-    if data == None:
+    if len(data) == 0:
         print("File not found in database, adding")
-        storing(filename, md5_hash, sha1_hash)
+        store_file_data(filename, md5_hash, sha1_hash)
         #stores the file and its data
     else:
         print("File exists in database!")
-        print()
-    
+        print(data)
 
-file_name()
-
-
+initial_setup()
